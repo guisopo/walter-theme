ed23b5028bf7dab4513e7b462e90b3dd85171164
guisopo <solerguillermo@hotmail.com>
2019-10-09 22:19:02 +0200

    Move work info into a figcaption inside figure in single posts

--- template-parts/single-image.php
+++ template-parts/single-image.php
@@ -12,23 +12,23 @@ $meta = walter_get_post_meta( get_the_ID() );
 
 <article class="work-wrapper">
 		
-	<?php
-		if( !empty($meta) ) {
-			?>
-			<div class="work__info">
-
-				<h2 class="work__title"><?php echo get_the_title(); ?></h2>
-				<p class="work__year"><?php echo $meta['date_completed'] ?></p>
-
-			</div>
-			<?php
-		}
-	?>
+	
 
 	<figure class="work__image-container">
 
 		<?php the_post_thumbnail( 'medium' ); ?>
+		<?php
+		if( !empty($meta) ) {
+			?>
+			<figcaption class="work__info">
 
+				<h2 class="work__title"><?php echo get_the_title(); ?></h2>
+				<p class="work__year"><?php echo $meta['date_completed'] ?></p>
+
+			</figcaption>
+			<?php
+		}
+	?>
 	</figure>
 
 	<nav class="work-nav">
