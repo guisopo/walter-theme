d8924809eda2bac2c1204c2b5c83eab465285fe9
guisopo <solerguillermo@hotmail.com>
2019-10-18 10:10:46 +0200

    Add condition to pre_gest_method action to run just in frontend

--- inc/walter-template-helpers.php
+++ inc/walter-template-helpers.php
@@ -24,8 +24,8 @@ function walter_site_name() {
  * @see pre_get_posts
  */
 function walter_main_custom_query( $query ) {
-  // Run only on the homepage
-  if ( $query->is_home() && $query->is_main_query() ) {
+  // Run only on the homepages
+  if ( ! is_admin() && $query->is_home() && $query->is_main_query() ) {
     $query->set( 'post_type', 'works' );
     $query->set( 'posts_per_page', 3 );
   }
