8eb5f8bcacbcc509d9876a7d3a0c5bf1ba34d4f2
guisopo <solerguillermo@hotmail.com>
2019-10-18 13:56:34 +0200

    Move ajax content to its own file

--- functions.php
+++ functions.php
@@ -20,36 +20,3 @@ function special_nav_class ($classes, $item) {
     }
     return $classes;
 }
-
-//AJAX
-
-add_action( 'wp_ajax_nopriv_load_more_posts', 'load_more_posts' );
-add_action( 'wp_ajax_load_more_posts', 'load_more_posts' );
-
-function load_more_posts() {
-
-    $next_page = $_POST['current_page'] + 1;
-    $query = new WP_Query([
-        'post_type' => 'works',
-        'posts_per_page' => 3,
-        'paged' => $next_page
-    ]);
-
-    if($query->have_posts()) : 
-
-        ob_start();
-        
-        while($query->have_posts()) : $query->the_post();
-
-            get_template_part( 'template-parts/content', get_post_format() );
-        
-        endwhile;
-
-        wp_send_json_success( ob_get_clean() );
-
-    else :
-
-        wp_send_json_error( 'No more posts' );
-
-    endif;
-}
