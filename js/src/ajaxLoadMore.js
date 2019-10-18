d32c21bf91f7213554fe7b87d053c40bb95b08b1
guisopo <solerguillermo@hotmail.com>
2019-10-18 17:52:25 +0200

    Delete button and call ajax just when current page is lower than max pages

--- js/src/ajaxLoadMore.js
+++ js/src/ajaxLoadMore.js
@@ -2,39 +2,33 @@ import axios from 'axios';
 
 const ajaxLoadMore = (scroll) => {
 
-  const button = document.querySelector('.load-more');
   const content = document.querySelector('.content-list');
 
-  if( typeof(button) === 'undefined' || button === null ) {
-    return;
-  }
-
-  button.addEventListener('click', (e) => {
     let current_page = content.dataset.page;
-    let params = new URLSearchParams();
+    let max_pages = content.dataset.max;
 
-    params.append( 'action', 'load_more_posts' );
-    params.append( 'current_page', current_page );
+    if(current_page < max_pages) {
 
-    axios.post('/wp-admin/admin-ajax.php', params)
-      .then( res => {
-
-        content.insertAdjacentHTML("beforeend", res.data.data);
-        content.dataset.page++;
-        
-        let baseUrl = '';
-        let getUrl = window.location;
-        baseUrl = `${getUrl.protocol}//${getUrl.host}/page/${content.dataset.page}`;
-        window.history.pushState('', '', baseUrl);
-
-        if (content.dataset.page === content.dataset.max) {
-          button.parentNode.removeChild(button);
-        }
-
-        scroll.setBounds();
-
-      });
-  });
+      let params = new URLSearchParams();
+  
+      params.append( 'action', 'load_more_posts' );
+      params.append( 'current_page', current_page );
+  
+      axios.post('/wp-admin/admin-ajax.php', params)
+        .then( res => {
+  
+          content.insertAdjacentHTML("beforeend", res.data.data);
+          content.dataset.page++;
+          
+          let baseUrl = '';
+          let getUrl = window.location;
+          baseUrl = `${getUrl.protocol}//${getUrl.host}/page/${content.dataset.page}`;
+          window.history.pushState('', '', baseUrl);
+  
+          scroll.setBounds();
+  
+        });
+    }
 }
 
 export default ajaxLoadMore;
