2e6743123edf880f1bdcda7b2cfd3dfb6e007789
guisopo <solerguillermo@hotmail.com>
2019-10-19 15:00:58 +0200

    Move filter to proper file

--- functions.php
+++ functions.php
@@ -12,12 +12,3 @@ require get_template_directory().'/inc/enqueue.php';
 require get_template_directory().'/inc/menu-management.php';
 require get_template_directory().'/inc/walter-template-helpers.php';
 require get_template_directory().'/inc/ajax-post-load.php';
-
-add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
-
-function special_nav_class ($classes, $item) {
-    if (in_array('current-menu-item', $classes) ){
-        $classes[] = 'active ';
-    }
-    return $classes;
-}
--- inc/menu-management.php
+++ inc/menu-management.php
@@ -1,3 +1,11 @@
 <?php
 
 register_nav_menu( 'primary', 'Main Navigation Menu' );
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
