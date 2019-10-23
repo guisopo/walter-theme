ef14a2c567744ac2092b73bb62ddb1d018ef7b68
guisopo <solerguillermo@hotmail.com>
2019-10-23 13:32:20 +0200

    Simplify and fix version removal

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -6,20 +6,13 @@
  */
 
 function walter_remove_wp_version( $src ) {
+  $parts = explode( '?ver', $src );
+  return $parts[0];
   /** 
    * Remove generator with wordpress version
    */
-  remove_action('wp_head', 'wp_generator'); 
-  /**
-   * Remove wordpress version from css and js files name strings
-   */
-	global $wp_version;
-	parse_str( parse_url($src, PHP_URL_QUERY), $query );
-	if ( !empty( $query['ver'] ) && $query['ver'] === $wp_version ) {
-		$src = remove_query_arg( 'ver', $src );
-	}
-	return $src;
+  remove_action('wp_head', 'wp_generator');
 }
 
-add_filter( 'script_loader_src', 'walter_remove_wp_version' );
-add_filter( 'style_loader_src', 'walter_remove_wp_version' );
+add_filter( 'script_loader_src', 'walter_remove_wp_version', 15, 1 );
+add_filter( 'style_loader_src', 'walter_remove_wp_version', 15, 1 );
