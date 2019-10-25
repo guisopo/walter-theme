1a3af43e2d8b0aeb0c110a71a60f3b2178bc0e96
guisopo <solerguillermo@hotmail.com>
2019-10-25 13:35:18 +0200

    Improve styles

--- js/src/doodle.js
+++ js/src/doodle.js
@@ -2,7 +2,7 @@ class Doodle {
 
   constructor() {
     // 1. Initial SVG
-    this.svg = ' <svg class="doodle" width="41px" height="82px" viewBox="0 0 41 82" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="iPhone-8" transform="translate(-6.000000, -302.000000)" stroke="#353232"><g id="Nav-Menu" transform="translate(6.000000, 302.000000)"><path class="path" stroke= “#000000” d="M41.5471152,56.2205478 C58.7195827,39.605151 57.3610014,28.811803 37.4713713,23.8405039 C-4.02223755,22.7317761 -23.0474477,30.2430441 -19.6042593,46.374308 C-14.4394766,70.5712038 50.0235756,48.2115805 58.9619536,41.7333944 C67.9003317,35.2552084 46.694508,18.7013327 9.91499511,21.2679874 C-26.8645177,23.834642 -15.7067613,58.1348619 9.91499511,60.7919565 C26.996166,62.563353 42.1293125,53.0227669 55.3144346,32.1701984" id="Path" transform="translate(20.500000, 41.000000) rotate(-90.000000) translate(-20.500000, -41.000000) "></path></g></g></g></svg>';
+    this.svg = '<svg class="doodle" width="73" height="48" viewBox="0 0 73 48" xmlns="http://www.w3.org/2000/svg"><g fill="none"><g stroke="#353232"><path class="path" d="M54.9 41.5C70 22.4 68.8 10 51.4 4.3 15 3-1.7 11.6 1.3 30.2 5.9 58 62.4 32.3 70.2 24.8 78 17.4 59.5-1.6 27.2 1.3 -5 4.3 4.8 43.7 27.2 46.8 42.2 48.8 55.5 37.8 67 13.8"/></g></g></svg>';
     // 2. Item to append SVG
     this.itemActive = document.querySelector('.active');
     // 3. SVG once appended to item
@@ -28,7 +28,8 @@ class Doodle {
   }
 
   addEvents() {
-    window.addEventListener( 'resize', this.setSvgPosition);
+    window.addEventListener('resize', this.init);
+    window.addEventListener('orientationchange', this.init);
   }
 
   appendSvg(element) {
@@ -56,6 +57,7 @@ class Doodle {
   }
 
   setSvgPosition() {
+    console.log('a');
     // 1. Get elements position
     const activeRect = this.itemActive.getBoundingClientRect();
     const svgRect = this.doodle.getBoundingClientRect();
