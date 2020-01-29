26f970647b615f376f4b223ce2f1ca89beaae12c
guisopo <solerguillermo@hotmail.com>
2020-01-29 16:19:04 +0100

    Realign code

--- template-parts/single-image.php
+++ template-parts/single-image.php
@@ -18,17 +18,17 @@ $meta = walter_get_post_meta( get_the_ID() );
 
 		<?php the_post_thumbnail( 'medium' ); ?>
 		<?php
-		if( !empty($meta) ) {
-			?>
-			<figcaption class="work__info">
+			if( !empty($meta) ) {
+				?>
+				<figcaption class="work__info">
 
-				<h2 class="work__title"><?php echo get_the_title(); ?></h2>
-				<p class="work__year"><?php echo $meta['date_completed'] ?></p>
+					<h2 class="work__title"><?php echo get_the_title(); ?></h2>
+					<p class="work__year"><?php echo $meta['date_completed'] ?></p>
 
-			</figcaption>
-			<?php
-		}
-	?>
+				</figcaption>
+				<?php
+			}
+		?>
 	</figure>
 
 	<nav class="work-nav">
